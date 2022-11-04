43b036d055d7bb8b330f7baec778a79d3d765ef1
IanRic32 <al2183041529@azc.uam.mx>
2022-11-03 22:30:12 -0600

    all files

---
+++ Cuenta.js
@@ -0,0 +1,39 @@
+export class Cuenta{
+    #cliente;
+    #saldo;
+
+    constructor(tipo,cliente, numero, agencia,saldo) {
+        this.tipo = tipo;
+        this.#cliente = cliente;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.#saldo = saldo;
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (this.tipo=='Coerriente')
+            valor = valor*1.05;
+        else if (this.tipo=='Ahorro')
+            valor = valor*1.02;
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+        valor = 200;
+        valor = valor*1000;
+    }
+}
