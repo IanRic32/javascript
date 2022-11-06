39111d0346da1553e4cfc041f227cf339d53afe4
IanRic32 <al2183041529@azc.uam.mx>
2022-11-06 15:15:07 -0600

    .

--- SistemaAutenticacion.js
+++ SistemaAutenticacion.js
@@ -1,5 +1,10 @@
 export class SistemaAutenticacion {
     static login (usuario,clave){
-        return usuario.autenticable(clave);
+        if ("autenticable" in usuario && usuario.autenticable instanceof Function ){
+            return usuario.autenticable(clave);
+            //instanceof es para ver si es funcion de la clase
+        }
+        else    
+            return false;
     }
 }
